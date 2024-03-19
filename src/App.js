08f9656292981a1964b57d6c0dd94ac54b653b79
anasAbounouar <anas.2002.abounouar@gmail.com>
2024-03-19 15:37:46 +0000

    fist commit coursera

--- src/App.js
+++ src/App.js
@@ -7,37 +7,31 @@ import Budget from './components/Budget';
 
 // Add code to import the other components here under
 
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { AppProvider } from './context/AppContext';
+import AllocationForm from './components/AllocationForm';
+import ExpenseList from './components/ExpenseList';
+import ExpenseTotal from './components/ExpenseTotal';
+import Remaining from './components/Remaining';
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
+                    <div className='d-flex flex-row justify-content-between'>
+                        <Budget />    
 
-                        {
-                            /* Add Remaining component here*/
-                        }        
+                 <Remaining/>       
 
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
-                            /* Add ExpenseList component here */
-                        }         
+                        <ExpenseTotal />
+                    </div>    
 
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
+                       <ExpenseList/>    
 
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
+{/*     <ExpenseItem/> */}
+
+                      <AllocationForm/>   
 
                 </div>
             </div>
--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,37 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget, dispatch } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+
+    const handleBudgetChange = (event) => {
+        setNewBudget(event.target.value);
+    }
+
+    const updateBudget = () => {
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: parseInt(newBudget, 10)
+        });
+    }
+
+   
+    const handleBlur = () => {
+        updateBudget();
+    }
+
+    return (
+        <div className='alert alert-secondary'>
+            <span>Budget: £</span>
+            <input
+                type="number"
+                step="10"
+                value={newBudget}
+                onChange={handleBudgetChange}
+                onBlur={handleBlur} // Update the budget when the input loses focus
+            ></input>
+        </div>
+    );
+};
+export default Budget;
